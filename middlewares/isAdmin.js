d203c2dc929b08b277c58847980f53c7476eebcb
Mikael <dev.kov.mtl@gmail.com>
2022-03-24 15:30:10 -0400

    Added isAdmin middleware check via jwt

---
+++ middlewares/isAdmin.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+exports.isAdmin = (req, res, next) => {
+  console.log(req.headers);
+  const bearerHeaders = req.headers['authorization'];
+  // if user exist token was sent with the request
+  if (bearerHeaders) {
+    const bearer = bearerHeaders.split(' ');
+    const bearerToken = bearer[1];
+    const decodedToken = jwt.decode(bearerToken);
+    if (decodedToken.isAdmin) {
+      next();
+    } else {
+      res.status(401).json({ error: 'You are not authorized' });
+    }
+  } else {
+    res.status(403).json({ error: 'You must login first.' });
+  }
+};
