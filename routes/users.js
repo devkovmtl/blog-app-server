a9d3e80f5e26e79b7a82193d4a4aae38d4a2a35f
Mikael <dev.kov.mtl@gmail.com>
2022-03-24 15:31:25 -0400

    Created profile routes to test auth and admin role

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const router = express.Router();
-
+const { Router } = require('express');
+const router = Router();
+const passport = require('passport');
+const { isAdmin } = require('../middlewares/isAdmin');
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.json({ user: 'user' });
-});
+router.get(
+  '/profile',
+  passport.authenticate('jwt', { session: false }),
+  isAdmin,
+  (req, res, next) => {
+    res.json({ user: 'user' });
+  }
+);
 
 module.exports = router;
